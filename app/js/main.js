c186b8c39a632091981bd345d840dde0c3fe9418
AlexOrinst <koluchiy8@gmail.com>
2020-03-17 21:59:16 +0300

    added 2 sections: search and faq

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,13 @@ $(function () {
         prevArrow: '<button type="button" class="slider__arrow-left icon-angle-left"></button>',
         nextArrow: '<button type="button" class="slider__arrow-right icon-angle-right"></button>'
     });
+
+    // $(".checkboxes input").on("click", function () {
+    //     if ($(".checkboxes input:checked").length >= 1) {// Не больше 2-х checkbox
+    //         $(".checkboxes input:not(:checked)").attr("disabled", true);
+    //     } else {
+    //         $(".checkboxes input:disabled").attr("disabled", false);
+    //     }
+
+    // });
 });
