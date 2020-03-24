d8264715980865b41e04ca3a38e1325224d4af8a
AlexOrinst <koluchiy8@gmail.com>
2020-03-24 20:16:50 +0300

    added howto

--- app/js/main.js
+++ app/js/main.js
@@ -17,5 +17,14 @@ $(function () {
         nextArrow: '<button type="button" class="slider__arrow-right icon-long-arrow-right"></button>'
     });
 
+    $('.howto__wrapper .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.howto__wrapper').find('.tab-item').removeClass('active-tab').hide();
+            $('.howto__wrapper .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
+
 });
 
